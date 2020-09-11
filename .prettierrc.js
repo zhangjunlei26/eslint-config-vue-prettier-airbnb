6e40d7e2235ca19af6e8b28c11f10a7e26de0d4e
rosenzhang <rosenzhang@tencent.com>
2020-09-11 11:56:12 +0800

    fix: prettier格式化定义与eslintrc中定义统一

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,2 @@
-module.exports = {
-  // 一行的字符数，如果超过会进行换行
-  printWidth: 100,
-  // tab 宽度
-  tabWidth: 2,
-  // 是否使用 tab 进行缩进，默认为 false
-  useTabs: false,
-  // 单引号字符串
-  singleQuote: true,
-  // 行尾是否使用分号，默认为 true
-  semi: true,
-  // 在多行的数组/对象的情况下，是否加上行尾逗号
-  trailingComma: 'none',
-  // 对象大括号直接是否有空格，默认为 true，效果：{ foo: bar }
-  bracketSpacing: true,
-  // 换行符，默认使用 linux 的 lf
-  endOfLine: 'lf'
-};
+let {rules:{'prettier/prettier':[_,config]}} = require('./index.js');
+module.exports = config;
